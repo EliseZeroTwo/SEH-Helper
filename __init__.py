1abdf92b0fd4f5103f915fd49cc09c441ab04ac4
Jordan Wiens <jordan@psifertex.com>
2017-09-05 15:47:24 -0400

    update to enums

--- __init__.py
+++ __init__.py
@@ -2,6 +2,6 @@ from binaryninja import *
 
 def do_nothing(bv,function):
 	show_message_box("Do Nothing", "Congratulations! You have successfully done nothing.\n\n" +
-					 "Pat yourself on the back.", OKButtonSet, ErrorIcon)
+					 "Pat yourself on the back.", MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.ErrorIcon)
 
 PluginCommand.register_for_address("Useless Plugin", "Basically does nothing", do_nothing)
