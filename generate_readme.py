75633d42becd27079d1e36769c86b79fe498b06f
Jordan Wiens <jordan@psifertex.com>
2018-08-21 14:51:21 -0400

    better unicode support via @yrp604

--- generate_readme.py
+++ generate_readme.py
@@ -3,6 +3,7 @@ import json
 import argparse
 import os
 import sys
+import io
 
 parser = argparse.ArgumentParser(description = 'Generate README.md (and optional LICENSE) from plugin.json metadata')
 parser.add_argument('filename', type = argparse.FileType('r'), help = 'path to the plugin.json file')
@@ -22,20 +23,16 @@ if not args.force and (os.path.isfile(outputfile) or os.path.isfile(licensefile)
 if 'license' in plugin and 'name' in plugin['license'] and 'text' in plugin['license']:
 	name = plugin['license']['name']
 	text = plugin['license']['text']
-	license = '''## License
-
+	license = u'''## License
 This plugin is released under a [{name}](LICENSE) license.
-
 '''.format(name=plugin['license']['name'])
 	print("Creating {licensefile}".format(licensefile=licensefile))
-	open(licensefile,'w').write(plugin['license']['text'])
+	io.open(licensefile,'w',encoding='utf8').write(plugin['license']['text'])
 
 elif ('license' in plugin and 'name' in plugin['license']):
 	name = plugin['license']['name']
-	license = '''## License
-
+	license = u'''## License
 	This plugin is released under a {name}] license.
-
 '''.format(name=plugin['license']['name'])
 else:
 	license = ''
@@ -44,29 +41,25 @@ if 'minimumBinaryNinjaVersion' in plugin:
 	minimum = '## Minimum Version\n\nThis plugin requires the following minimum version of Binary Ninja:\n\n'
 	for chan in plugin['minimumBinaryNinjaVersion']:
 		version = plugin['minimumBinaryNinjaVersion'][chan]
-		minimum += " * {chan} - {version}\n".format(chan = chan, version = version)
+		minimum += u" * {chan} - {version}\n".format(chan = chan, version = version)
 	minimum += '\n'
 else:
 	minimum = ''
 
 if 'dependencies' in plugin:
-	dependencies = '## Required Dependencies\n\nThe following dependencies are required for this plugin:\n\n'
+	dependencies = u'## Required Dependencies\n\nThe following dependencies are required for this plugin:\n\n'
 	for dependency in plugin['dependencies']:
-		dependencylist = ', '.join(plugin['dependencies'][dependency])
-		dependencies += " * {dependency} - {dependencylist}\n".format(dependency = dependency, dependencylist = dependencylist)
+		dependencylist = u', '.join(plugin['dependencies'][dependency])
+		dependencies += u" * {dependency} - {dependencylist}\n".format(dependency = dependency, dependencylist = dependencylist)
 	dependencies += '\n'
 else:
 	dependencies = ''
 
-template = '''# {PluginName} (v{version})
+template = u'''# {PluginName} (v{version})
 Author: **{author}**
-
 _{description}_
-
 ## Description:
-
 {longdescription}
-
 {minimum}
 {dependencies}
 {license}
@@ -76,4 +69,5 @@ _{description}_
 		   dependencies = dependencies, minimum = minimum)
 
 print("Writing {outputfile}".format(outputfile=outputfile))
-open(outputfile, 'w').write(template)
+io.open(outputfile, 'w', encoding='utf8').write(template)
+
